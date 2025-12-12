8d1512944702f1a144cc06bc4224db9c2a6ac604
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-12-12 08:29:03 -0600

    Started implementing person

--- person.py
+++ person.py
@@ -1,12 +1,25 @@
+from schedule import Skd
+
 class Person:
-  def __init__(self, name):
-    self.__name = name
-    self.__food = 100
-    self.__water = 100
-  def feed(self, amount: int):
+  def __init__(self, name: str):
+    self.__name: str = name
+    self.__food: float = 100
+    self.__water: float = 100
+    self.__skd: list[Skd] = []
+    self.__state: int = 0
+  
+  def feed(self, amount: float) -> float:
     self.__food += amount
-  def starve(self, amount: int):
+    return self.__food
+  def starve(self, amount: float) -> float:
     self.__food -= amount
+    return self.__food
+  def skdAdd(self, entry: Skd) -> list[Skd]:
+    self.__skd.append(entry)
+    return self.__skd
+  def skdRem(self, entry: Skd) -> list[Skd]:
+    self.__skd.remove(entry)
+    return self.__skd
   
   def name(self) -> str:
     """
@@ -17,21 +30,40 @@ class Person:
     :rtype: str
     """
     return self.__name
-  def food(self) -> int:
+  def food(self) -> float:
     """
     Returns the current percent food level of the person.
     
     :param self: This person.
     :return: The food level.
-    :rtype: int
+    :rtype: float
     """
     return self.__food
-  def water(self) -> int:
+  def water(self) -> float:
     """
     Returns the current percent water level of the person.
     
     :param self: This person.
     :return: The water level.
-    :rtype: int
+    :rtype: float
     """
     return self.__water
+  def skd(self) -> list[Skd]:
+    return self.__skd
+  def die(self):
+    # need implementation within unreal (nitro python runtime could work)
+    self.__state = 1
+    print("dead")
+  
+  def upd(self):
+    if self.__state == 1: return
+    elif self.__food <= 0 or self.__water <= 0:
+      self.die()
+
+  def __str__(self):
+    return f"-- Person --\nName: \"{self.__name}\"\nFood: {self.__food}\nWater: {self.__water}"
+
+person = Person("Hello World")
+print(person)
+person.starve(100)
+person.upd()
---
+++ schedule.py
@@ -0,0 +1,5 @@
+class Skd:
+    def __init__(self, name: str):
+        self.__name: str = name
+    def name(self) -> str:
+        return self.__name
